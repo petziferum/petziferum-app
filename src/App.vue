588768b09b0efee7fcd94e23c34b78d559ce277f
petziferum <daboarderpjb@gmail.com>
2020-01-18 12:48:12 +0100

    logo

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,10 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-img :src="logo" contain max-width="140px"></v-img>
+      <span class="headline"> - APP</span>
+<v-spacer></v-spacer>
     </v-app-bar>
 
     <v-content>
@@ -48,12 +22,8 @@
 export default {
   name: 'App',
 
-  components: {
-
-  },
-
   data: () => ({
-    //
+    logo: require("@/assets/Petziferum_schriftzug_Trans.png")
   }),
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-col class="border " >
+    <vRow class="d-flex justify-center" >
+<v-col cols="2" class="border">
+  <h1>title</h1>
+</v-col>
+      <v-col cols="4" class="border">
+        <h1>title</h1>
+      </v-col>
+      <v-col cols="2" class="border">
+        <h1>title</h1>
+      </v-col>
+    </vRow>
+  </v-col>
+
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
+<style>
+  .border {
+    border:1px solid
+  }
+</style>
